a7aa2343bf8ae39edc1971e450ecddacf8f736f4
Jamila <135280536+potpot89@users.noreply.github.com>
2024-01-12 23:33:45 +0100

    made javascript to add elements to the dom for each category. implementing the code to remove the divs when pressing on new cat

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
     V creo i pulsanti per ogni categoria
     V inserisco i pulsanti nel DOM
     V creo il listener per i pulsanti
-    - ogni pulsante chiama la stessa funzione, ma con parametri diversi
+    V ogni pulsante chiama la stessa funzione, ma con parametri diversi
     - questa funzione farà la fetch all'endpoint per ottenere i prodotti della categoria specificata come argomento
     - faccio un ciclo per generare un l'elenco di prodotti nel DOM
     - per ogni prodotto potrei mostrare titolo, descrizione, prezzo e foto
@@ -24,10 +24,32 @@ fetch("https://fakestoreapi.com/products/categories")
       categoryButton.textContent = cat;
       mainHTML.appendChild(categoryButton);
 
-      //addEventListener to each button
-      categoryButton.addEventListener(`click`, hello);
-      function hello() {
-        return alert(`hello`);
+      //addEventListener to each button so that each of them calls the same function, that will have different parameters
+      categoryButton.addEventListener(`click`, getProduct);
+      function getProduct() {
+        let categoryLink = "https://fakestoreapi.com/products/category/" + cat;
+        return fetch(categoryLink)
+          .then((res) => res.json())
+          .then((json) => {
+            console.log(json);
+            json.forEach((product) => {
+              console.log({ product });
+
+              //for each product add a div in the <main>
+
+              const objContainer = document.createElement("div");
+              mainHTML.appendChild(objContainer);
+
+              //for each product return the key, value pairs in a div
+
+              Object.entries(product).forEach(([key, value]) => {
+                const h3 = document.createElement("h3");
+                h3.textContent = `${key}: ${value}`;
+                objContainer.appendChild(h3);
+              });
+            });
+          });
+        //remove the div when clicking another button
       }
     });
   });
